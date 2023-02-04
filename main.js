fba563936b1985d94945beb19c5a5ea53f88726f
Loai <43700105+Loai106@users.noreply.github.com>
2023-02-04 22:51:36 +0300

    modifing the cross sign and its functionality

--- main.js
+++ main.js
@@ -6,10 +6,14 @@ const todoSection = document.getElementById('todos');
 function addTask(task)
 {
     let htmlSentence = ` <div class="todo-box todo-wrapper">
-    <div class="check-circle circle-notChecked added"></div>
-    <p class="task added">${task}</p>
-    <div class="cross"> <svg class=xmlns="http://www.w3.org/2000/svg" width="18" height="18"><path fill="hsl(236, 9%, 61%)" fill-rule="evenodd" d="M16.97 0l.708.707L9.546 8.84l8.132 8.132-.707.707-8.132-8.132-8.132 8.132L0 16.97l8.132-8.132L0 .707.707 0 8.84 8.132 16.971 0z"/></svg>
-    </div>     
+    <div class="check-circle circle-notChecked added-circle"></div>
+    <div class="task added-task">${task}
+      <span class="cross">
+      <svg id="delete-task" class=xmlns="http://www.w3.org/2000/svg" width="18" height="18">
+      <path fill="hsl(236, 9%, 61%)" fill-rule="evenodd" d="M16.97 0l.708.707L9.546 8.84l8.132 8.132-.707.707-8.132-8.132-8.132 8.132L0 16.97l8.132-8.132L0 .707.707 0 8.84 8.132 16.971 0z"/></svg>
+      </span>
+    </div>
+      
   </div>`
 
     //appending the task to the todo section
@@ -33,11 +37,33 @@ document.addEventListener('click',e=>{
     const target = e.target;
     //the pervious sibling of list items
 
-    if(target.classList.contains("added"))
+    if(target.classList.contains("added-task"))
     {
         const unCheckedCircle = target.previousElementSibling;
         target.classList.toggle("completed-task");
         unCheckedCircle.classList.toggle("circle-checked");
     }
+    else if(target.classList.contains("added-circle")){
+        const unCheckedTask = target.nextElementSibling;
+        console.log(unCheckedTask);
+        target.classList.toggle("circle-checked");
+        unCheckedTask.classList.toggle("completed-task");
+    }
 });
 
+
+//deleting the task after clicking the cross 
+document.addEventListener('click',e=>{
+
+    const target = e.target;
+    console.log(target.id);
+    if(target.id == 'delete-task')
+    {
+        const parentNode = target.parentNode.parentNode.parentNode;
+        parentNode.parentNode.removeChild(parentNode);
+    }
+    
+        
+
+
+});
